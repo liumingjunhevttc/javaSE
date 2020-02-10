c5e4ead8b7649e5e7f833c52b37b0967237ed777
technology_liu <2252904046@qq.com>
2020-02-10 20:51:50 +0800

    初始化

---
+++ java-01/src/main/java/com/baidu/FirstSample.java
@@ -0,0 +1,12 @@
+package com.baidu;
+
+/**
+ * java 程序基本结构
+ * @author technology_liu
+ * @since 1.8
+ */
+public class FirstSample {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ java-01/src/main/java/com/baidu/SecondSample.java
@@ -0,0 +1,23 @@
+package com.baidu;
+
+/**
+ *  java 注释
+ * @author 刘明君
+ * @since 1.8
+ */
+public class SecondSample {
+    public static void main(String[] args) {
+
+        // 输出一个 hello world(单行注释)
+        System.out.println("hello world");
+        /*
+            输出一个hello world(多行注释)
+         */
+        System.out.println("hello world");
+
+        /**
+         * 输出一个hello world
+         */
+        System.out.println("hello world");
+    }
+}
