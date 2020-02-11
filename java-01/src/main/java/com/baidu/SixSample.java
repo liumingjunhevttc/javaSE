3a3b1c5320b66129f78ea6f740be8307591a6588
technology_liu <2252904046@qq.com>
2020-02-11 22:12:26 +0800

    基本数据类型  char类型

---
+++ java-01/src/main/java/com/baidu/SixSample.java
@@ -0,0 +1,17 @@
+package com.baidu;
+
+/**
+ * 基本数据类型 -- 布尔类型
+ * @author 刘明君
+ * 2020-02-11
+ */
+public class SixSample {
+    public static void main(String[] args) {
+        // 真
+        boolean flag = true;
+        System.out.println("真:"+flag);
+        // 假
+        boolean flag1 = false;
+        System.out.println("假:"+flag1);
+    }
+}
