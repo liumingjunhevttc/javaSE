cac6444447b6e13b3899dc3a2eadb433b7acb924
technology_liu <2252904046@qq.com>
2020-02-12 18:38:47 +0800

    数学函数

---
+++ java-01/src/main/java/com/baidu/NineSample.java
@@ -0,0 +1,20 @@
+package com.baidu;
+
+/**
+ * 数学函数
+ * @author 刘明君
+ * 2020-02-12
+ */
+public class NineSample {
+    public static void main(String[] args) {
+        // 平方根
+        double x= 4;
+        System.out.println("平方根"+Math.sqrt(4));
+        // 幂运算
+        System.out.println("幂运算:"+Math.pow(2,3));
+        // pai
+        System.out.println("pai:"+Math.PI);
+        // E
+        System.out.println("E:"+Math.E);
+    }
+}
