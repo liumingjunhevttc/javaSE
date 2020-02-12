015927af4b216ca5ea5efa7b0e5d43e6b0db60e3
technology_liu <2252904046@qq.com>
2020-02-12 18:30:05 +0800

    运算符

---
+++ java-01/src/main/java/com/baidu/EightSample.java
@@ -0,0 +1,16 @@
+package com.baidu;
+
+/**
+ * 运算符
+ * @author technology_liu
+ * 2020-02-12
+ */
+public class EightSample {
+    public static strictfp void main(String[] args) {
+        System.out.println("加法:"+(1+1));
+        System.out.println("减法:"+(5-3));
+        System.out.println("乘法:"+(5*3));
+        System.out.println("除法:"+(5/3));
+        System.out.println("取余:"+(5.0/3));
+    }
+}
