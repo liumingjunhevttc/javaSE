8ef3c66b4717984a1780661393d8346c688e66ee
technology_liu <2252904046@qq.com>
2020-02-22 20:42:20 +0800

    块作用域

---
+++ java-01/src/main/java/com/baidu/BlockScope.java
@@ -0,0 +1,15 @@
+package com.baidu;
+
+public class BlockScope {
+    public static void main(String[] args) {
+        int n;
+        {
+            int k;
+        }
+
+//        int i;
+//        {
+//            int i;
+//        }
+    }
+}
