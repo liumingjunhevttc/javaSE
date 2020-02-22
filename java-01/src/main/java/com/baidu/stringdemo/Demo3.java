84f084981cb07018a636596ce2c8ad0266541bee
technology_liu <2252904046@qq.com>
2020-02-22 20:33:49 +0800

    StringBuilder

---
+++ java-01/src/main/java/com/baidu/stringdemo/Demo3.java
@@ -0,0 +1,10 @@
+package com.baidu.stringdemo;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("hello");
+        stringBuilder.append("world");
+        System.out.println(stringBuilder.toString());
+    }
+}
