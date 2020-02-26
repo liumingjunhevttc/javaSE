88d913b6c8cbb53a2d02d9471b24641759465069
technology_liu <2252904046@qq.com>
2020-02-26 21:39:23 +0800

    命令行参数

---
+++ java-01/src/main/java/com/baidu/CommandDemo.java
@@ -0,0 +1,9 @@
+package com.baidu;
+
+public class CommandDemo {
+    public static void main(String[] args) {
+        for (String str:args){
+            System.out.println(str);
+        }
+    }
+}
