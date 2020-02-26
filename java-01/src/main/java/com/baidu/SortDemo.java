0fa2d32a99c0d8a2002f3bd2da050beb19b16c24
technology_liu <2252904046@qq.com>
2020-02-26 21:45:53 +0800

    Arrays sort 快速排序

---
+++ java-01/src/main/java/com/baidu/SortDemo.java
@@ -0,0 +1,11 @@
+package com.baidu;
+
+import java.util.Arrays;
+
+public class SortDemo {
+    public static void main(String[] args) {
+        int[] a = {2,6,5,4,7,8,12,23};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
