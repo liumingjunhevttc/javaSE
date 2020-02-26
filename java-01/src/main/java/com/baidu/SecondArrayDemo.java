83ca0950bb25fa2b658130a64b6ba74ad81a223e
technology_liu <2252904046@qq.com>
2020-02-26 23:09:23 +0800

    多维数组

---
+++ java-01/src/main/java/com/baidu/SecondArrayDemo.java
@@ -0,0 +1,10 @@
+package com.baidu;
+
+public class SecondArrayDemo {
+    public static void main(String[] args) {
+        double[][] salary;
+        salary = new double[2][2];
+        int[][] salary1 = {{16,3,2,13},
+                            {16,3,2,13}};
+    }
+}
