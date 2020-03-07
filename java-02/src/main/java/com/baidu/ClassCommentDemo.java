bca3b3f3b64380a15e95ae70a7e5d34870b41cd0
technology_liu <2252904046@qq.com>
2020-03-07 22:12:31 +0800

    初始化代码块

---
+++ java-02/src/main/java/com/baidu/ClassCommentDemo.java
@@ -0,0 +1,24 @@
+package com.baidu;
+
+/**
+ * 这是一个类注释
+ * @author 刘明君
+ * 2020-03-05
+ */
+public class ClassCommentDemo {
+
+    /**
+     * 这是域变量
+     */
+    public static final boolean flag = true;
+    /**
+     * 加法
+     * @param a
+     * @param b
+     * @return a+b
+     * @throws Exception
+     */
+    public int add(int a,int b){
+        return a+b;
+    }
+}
