161d46672a239168763480257f42998d291486ee
technology_liu <2252904046@qq.com>
2020-03-12 20:17:42 +0800

    包装类

---
+++ java-03/src/main/java/com/baidu/IntegerAPIDemo.java
@@ -0,0 +1,10 @@
+package com.baidu;
+
+public class IntegerAPIDemo {
+    public static void main(String[] args) {
+        Integer i =20;
+        System.out.println(i.intValue());
+        System.out.println(i.toString());
+        System.out.println(i.toString(5,2));
+    }
+}
